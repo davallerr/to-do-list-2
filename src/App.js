0786814a4bb55b288d13f343a07dd3cf7dbafd51
David Kerr <davallerr@gmail.com>
2017-07-20 18:04:54 -0700

    start testing add own components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Task extends React.Component {
+  render() {
+    return (
+      <div id='task-0' className='task'>
+        <div className='task__description'>task description</div>
+        <div className='task__date'>xx/xx/xxxx</div>
+        <div className='task__delete'><button className='task__delete--btn'>delete</button></div>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +25,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Task />
       </div>
     );
   }
